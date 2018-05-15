f7aa13c5b03d8dca2b2b641ef55e8b6e9382c5b7
Banou26 <banoushirosaki@gmail.com>
2018-05-15 21:30:11 +0200

    Improve index card overview

--- src/documentation/overview.js
+++ src/documentation/overview.js
@@ -10,18 +10,19 @@ const style = ({state: { id }}) => css\`
   display: block;
   background-color: white;
   padding: 2rem;
-  border-radius: .3rem;
+  border-radius: .2rem;
 }
 
 .placeholder {
   height: 18px;
-  background-color: grey;
+  background-color: hsla(0, 0%, 0%, .5);
+  border-radius: .2rem;
 }
 \`
 
 const template = ({state: {id}, props: {name}}) => poz\`
 .name \${name}
-.id(class="\${id && id.$resolved !== false ? '' : 'placeholder'}") \${id}
+.id.\${id && id.$resolved !== false ? '' : 'placeholder'} \${id}
 \`
 
 registerElement({
@@ -30,10 +31,13 @@ registerElement({
   state: _ => ({ id: undefined }),
   template,
   style,
-  watchers: [({state, state: {name}}) => state ? state.id = externalAPI('Card 1') : undefined]
+  watchers: [({state, props: {name}}) => name ? state.id = externalAPI('Card 1') : undefined]
 })
+
 const card = document.createElement('app-card')
 card.name = 'Card 1'
 document.body.appendChild(card)
+
+setInterval(_ => card.name = \`Card \${Number(card.name.slice(5)) + 1}\`, 2000)
 `
 
